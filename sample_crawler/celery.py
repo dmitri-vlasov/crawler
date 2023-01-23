bd0506598d25d021e41da4e51d827af325935342
Dmitry Vlasov <dmitri.vlasov@gmail.com>
2023-01-23 22:27:39 +0400

    unused redis initialization removed

--- sample_crawler/celery.py
+++ sample_crawler/celery.py
@@ -1,12 +1,10 @@
 import logging
 import os
 
-import redis
 from celery import Celery
 from django.conf import settings
 
 logger = logging.getLogger('django')
-redis_client = redis.Redis()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_crawler.settings')
 
